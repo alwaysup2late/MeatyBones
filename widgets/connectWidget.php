9d52a7ca685ef3c321a1933e92a08838d8bf79c7
alwaysup2late <jonathan@alwaysup2late.com>
2013-07-30 12:36:50 -0400

    rebuilt and included widget

--- widgets/connectWidget.php
+++ widgets/connectWidget.php
@@ -1,100 +1,101 @@
 <?php
-/**
- * Plugin Name: Connect Widget
- * Description: A widget that displays social icons and links.
- * Version: 1.0
- * Author: Jonathan Davis
- * Author URI: http://www.varioinc.com/
- */
+/* Plugin Name: MeatyBones Connect Widget
+Plugin URI: https://github.com/alwaysup2late/MeatyBones
+Description: [Enter brief description of plugin]
+Version: 1.0
+Author: Jonathan Davis
+Author URI: http://www.alwaysup2late.com/
+*/
 
-class Connect_Widget extends WP_Widget {
-	function My_Widget() {
-		$widget_ops = array(
-			'classname' => 'connect',
-			'description' => __('A widget that displays the authors name ', 'connect')
-		);
-		
-		$control_ops = array(
-			'width' => 300, 
-			'height' => 350,
-			'id_base' => 'connect-widget'
-		);
-		
-		$this -> WP_Widget('connect-widget', __('Connect Widget', 'connect'), $widget_ops, $control_ops);
-	}
+class mb_connectWidget extends WP_Widget {
+     function mb_connectWidget() {
+          $widget_ops = array(
+               'classname' => 'mb_connectWidget',
+               'description' => 'A Widget to display social links defined in Theme Options.'
+          );
 
-	function widget($args, $instance) {
-		extract($args);
+          $this->WP_Widget(
+               'mb_connectWidget',
+               'MB Connect Widget',
+               
+               $widget_ops
+          );
+     }
 
-		$title = apply_filters('widget_title', $instance['title']);
+     function widget($args, $instance) {
+          extract($args, EXTR_SKIP);
 
-		echo $before_widget;
+          $title = apply_filters('widget_title', $instance['title']);
+          
+          echo '<div class="connectWidget">'.$before_widget;
 
-		if ($title) {
-			echo $before_title . $title . $after_title;			
-		}
+          if ($title) {
+               echo $before_title . $title . $after_title;            
+          }
+          
+          echo '<ul>';
 
-		echo '<ul>';
-		
-		if (get_option('mb_facebook_link')!="") {
-			echo '<li><a href="';
-			echo get_option('mb_facebook_link');
-			echo '" class="facebook" target="_blank">Facebook</a></li>';
-		}
+          if (get_option('mb_main_email') !="") {
+               echo '<li><a href="mailto:';
+               echo get_option('mb_main_email');
+               echo '?subject=';
+               echo get_option('mb_main_email_subject');
+               echo'" class="email">Email</a></li>';
+          }
 
-		if (get_option('mb_twitter_user')!="") {
-			echo '<li><a href="http://www.twitter.com/';
-			echo get_option('mb_twitter_user');
-			echo '" class="twitter" target="_blank">Twitter</a></li>';
-		}
+          if (get_option('mb_facebook_link') != "") {
+               echo '<li><a href="';
+               echo get_option('mb_facebook_link');
+               echo '" class="facebook" target="_blank">Facebook</a></li>';
+          }
 
-		if (get_option('mb_linkedin_link')!="") {
-			echo '<li><a href="';
-			echo get_option('mb_linkedin_link');
-			echo '" class="linkedin" target="_blank">LinkedIn</a></li>';
-		}
+          if (get_option('mb_twitter_user') != "") {
+               echo '<li><a href="http://www.twitter.com/';
+               echo get_option('mb_twitter_user');
+               echo '" class="twitter" target="_blank">Twitter</a></li>';
+          }
 
+          if (get_option('mb_linkedin_link') != "") {
+               echo '<li><a href="';
+               echo get_option('mb_linkedin_link');
+               echo '" class="linkedin" target="_blank">LinkedIn</a></li>';
+          }
 
-		echo '<li><a href="';
-		echo bloginfo('rss2_url');
-		echo '" title="RSS" class="rss">RSS</a></li>';
+          if (get_option('mb_display_rss') == "yes") {
+               echo '<li><a href="';
+               echo bloginfo('rss2_url');
+               echo '" title="RSS" class="rss">RSS</a></li>';               
+          }
 
-		echo '</ul>';
+          echo '</ul>';
 
-		echo $after_widget;
-	}
-	
-	function update($new_instance, $old_instance) {
-		$instance = $old_instance;
+          echo $after_widget.'</div>';
+     }
 
-		//Strip tags from title and name to remove HTML
-		$instance['title'] = strip_tags($new_instance['title']);
+     function update($new_instance, $old_instance) {
+          $instance = $old_instance;
 
-		return $instance;
-	}
+          $instance['title'] = strip_tags($new_instance['title']);
 
-	function form($instance) {
-		//Set up some default widget settings.
-		$defaults = array(
-			'title' => __(
-				'connect',
-				'connect'
-			)
-		);
+          return $instance;
+     }
 
-		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+     function form($instance) {
+          $defaults = array(
+               'title' => __(
+                    ''
+               )
+          );
 
-		<p>
-			<label for="<?php echo $this -> get_field_id('title'); ?>"><?php _e('Title:', 'connect'); ?></label>
-			<input id="<?php echo $this -> get_field_id('title'); ?>" name="<?php echo $this -> get_field_name('title'); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
-		</p>
+          $instance = wp_parse_args( (array) $instance, $defaults ); ?>
 
-		<p>This widget will display the social links that are configurable under the <a href="<?php bloginfo('url'); ?>/wp-admin/admin.php?page=functions.php">Theme Options</a> page.</p>
-	<?php }
+          <p>
+               <label for="<?php echo $this -> get_field_id('title'); ?>"><?php _e('Title:', 'connect'); ?> (optional)</label>
+               <input id="<?php echo $this -> get_field_id('title'); ?>" name="<?php echo $this -> get_field_name('title'); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+          </p>
+
+          <p>This widget will display the social links that are configurable under the <a href="<?php bloginfo('url'); ?>/wp-admin/admin.php?page=functions.php">Theme Options</a> page.</p>
+     <?php }
 }
 
-function register_connect_widget() {
-	register_widget('Connect_Widget');
-}
-
-add_action('widgets_init', 'register_connect_widget');
+add_action('widgets_init', create_function('','return register_widget("mb_connectWidget");'));
