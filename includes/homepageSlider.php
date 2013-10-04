3b8b887c725d9e3e0fa87a03cda237c5af636104
alwaysup2late <jonathan@alwaysup2late.com>
2013-10-03 20:10:31 -0400

    Added labels for homepage sliders

--- includes/homepageSlider.php
+++ includes/homepageSlider.php
@@ -8,6 +8,18 @@ function create_homepage_slider() {
 		array(
 			'public' => true,
 			'label' => 'Home Slides',
+			'labels' => array(
+				'name' => 'Home Slides',
+				'singular_name' => 'Slide',
+				'add_new' => 'Add New Slide',
+				'add_new_item' => 'Add New Slide',
+				'edit_item' => 'Edit Slide',
+				'new_item' => 'Add New Slide',
+				'view_item' => 'View Slide',
+				'search_items' => 'Search Slide',
+				'not_found' => 'No Slides found',
+				'not_found_in_trash' => 'No slides found in trash'
+			),
 			'supports' => array(
 				'title',
 				'thumbnail',
